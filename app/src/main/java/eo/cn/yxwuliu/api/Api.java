76edb1ae03f48c55aaa45b40d01330316524a766
break396 <1298587604@qq.com>
2017-05-27 09:49:46 +0800

    这个一个基于mvp设计模式的简单的基础框架，很基础

---
+++ app/src/main/java/eo/cn/yxwuliu/api/Api.java
@@ -0,0 +1,9 @@
+package eo.cn.yxwuliu.api;
+
+/**
+ * Created by mk on 2017/5/26.
+ */
+
+public class Api {
+    public static String baseUrl = "";
+}
---
+++ app/src/main/java/eo/cn/yxwuliu/server/IHttpInterface.java
@@ -0,0 +1,8 @@
+package eo.cn.yxwuliu.server;
+
+/**
+ * Created by mk on 2017/5/26.
+ */
+
+public interface IHttpInterface {
+}
